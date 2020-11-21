a92dcbb5830163dfef1340ccc1b21cdf843553a8
wednesdayxx <markromig.09@gmail.com>
2020-11-21 13:57:50 +1100

    updated ddl

--- coffee-shout-practice/database/ddl.sql
+++ coffee-shout-practice/database/ddl.sql
@@ -1,9 +1,7 @@
 drop table if exists [user];
 drop table if exists coffeeDate;
 
-create table [user] (
-    firstName   NVARCHAR(20),
-    lastname    NVARCHAR(20),
+create table [user] (   
     username    NVARCHAR(20),
     [password]    NVARCHAR(20),
     primary key (username)
