6cecf23ead206dbdd6836f9cd5ad4281c2549393
wednesdayxx <markromig.09@gmail.com>
2020-12-02 14:02:05 +1100

    cleaned up projects

--- coffee/coffeeAPI/coffeeAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace coffeeAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- coffee/coffeeAPI/coffeeAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace coffeeAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
